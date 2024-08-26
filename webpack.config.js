f0964a26f12842cde4c50c241dccb864bd57aed1
SenselessObsessed <toni.pakhomov.2015@mail.ru>
2024-08-26 19:35:26 +0400

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,49 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+              test: /\.(png|svg|jpg|jpeg|gif)$/i,
+              type: 'asset/resource',
+            },
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/i,
+              type: 'asset/resource',
+            }
+        ]
+    },
     mode: 'production',
-    entry : path.resolve(__dirname, 'src', 'index.js'),
-    output : {
-        path: path.resolve(__dirname, 'bundle'),
-        filename: 'main.js'
+    devServer: {
+        port: 3000,
+        open: true
     }
 }
